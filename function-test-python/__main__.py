e6b98eb83d5cd5930fb1fc9c91214bee99861cea
Jun-Iwasaki <53922934+Jun-Iwasaki@users.noreply.github.com>
2025-04-20 13:39:29 +0900

    Update __main__.py

--- function-test-python/__main__.py
+++ function-test-python/__main__.py
@@ -21,12 +21,11 @@ import ibm_db
 import requests
 import os
 def main(dict):
-    result_body = {'result': [(111, '山田', 45, 'M', '03-3333-4444', '中央区日本橋箱崎町 19-21', 'マルゲリータ', 'L', 'コーラ', '唐揚げ', 'P', None)]}
     ssldsn = "DATABASE=BLUDB;HOSTNAME=a25e0deb-8892-4080-8646-5f11a321fed6-jptok.bt1ibm.db2.ibmappdomain.cloud;PORT=30227;PROTOCOL=TCPIP;UID=cfe1c59d;PWD=u3f2tO8D4RlgsPAI;Security=SSL"
     db_conn = ibm_db.connect(ssldsn,"","")
     sql = "SELECT * FROM ACCOUNT.ACCOUNT_DATA WHERE ID = ?"
     db_stmt = ibm_db.prepare(db_conn,sql)
-    id = "111"
+    id = "333"
     ibm_db.bind_param(db_stmt,1,id)
     ibm_db.execute(db_stmt)
     rows = ibm_db.fetch_tuple(db_stmt)
